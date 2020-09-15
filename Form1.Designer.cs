69580c8477665519ed58af72dbe3d285f1d9b161
ALI ZIA KHAN <azk443.443@gmail.com>
2020-09-16 03:24:09 +0500

    This is a game using dynamic controls along with manual coding and tools-A monster kilss aliens and hence scre is increased

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace AssignmentoopQ1
+﻿namespace ASSIGNMENTOOPQ3
 {
     partial class Form1
     {
@@ -30,110 +30,109 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.trackBar2 = new System.Windows.Forms.TrackBar();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.timer2 = new System.Windows.Forms.Timer(this.components);
-            this.timer3 = new System.Windows.Forms.Timer(this.components);
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
-            // 
-            // trackBar1
-            // 
-            this.trackBar1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.trackBar1.Location = new System.Drawing.Point(3, 152);
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(1014, 45);
-            this.trackBar1.TabIndex = 0;
-            // 
-            // trackBar2
-            // 
-            this.trackBar2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.trackBar2.Location = new System.Drawing.Point(-1, 394);
-            this.trackBar2.Name = "trackBar2";
-            this.trackBar2.Size = new System.Drawing.Size(1018, 45);
-            this.trackBar2.TabIndex = 1;
-            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(3, 72);
+            this.pictureBox1.Location = new System.Drawing.Point(786, 166);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(131, 74);
+            this.pictureBox1.Size = new System.Drawing.Size(118, 141);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(3, 316);
+            this.pictureBox2.Location = new System.Drawing.Point(69, 40);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(136, 72);
+            this.pictureBox2.Size = new System.Drawing.Size(93, 104);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(115, 295);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(89, 104);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(274, 153);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(90, 104);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(361, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 35);
+            this.label1.TabIndex = 4;
             // 
             // timer1
             // 
+            this.timer1.Interval = 150;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // timer2
             // 
             this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
-            // timer3
-            // 
-            this.timer3.Enabled = true;
-            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
-            // 
-            // label1
-            // 
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Maiandra GD", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label1.Location = new System.Drawing.Point(338, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(427, 32);
-            this.label1.TabIndex = 4;
-            // 
-            // label2
-            // 
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Sitka Text", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Location = new System.Drawing.Point(279, 209);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(493, 42);
-            this.label2.TabIndex = 5;
-            // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.Font = new System.Drawing.Font("Castellar", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(32, 209);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(873, 52);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(102, 32);
-            this.button1.TabIndex = 6;
+            this.button1.Size = new System.Drawing.Size(93, 35);
+            this.button1.TabIndex = 5;
             this.button1.Text = "START";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(873, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 37);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
@@ -141,38 +140,36 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1018, 541);
+            this.ClientSize = new System.Drawing.Size(1031, 536);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.trackBar2);
-            this.Controls.Add(this.trackBar1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TrackBar trackBar1;
-        private System.Windows.Forms.TrackBar trackBar2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Timer timer2;
-        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,8 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
-namespace AssignmentoopQ1
+namespace ASSIGNMENTOOPQ3
 {
     public partial class Form1 : Form
     {
@@ -16,25 +17,65 @@ namespace AssignmentoopQ1
         {
             InitializeComponent();
         }
-        double car1_speed = 0;
-        double car2_speed = 0;
-        double distance_to_be_covered=920;
-        double car1_time=0;
-        double car2_time = 0;
-
-        private void trackBar2_Scroll(object sender, EventArgs e)
-        {
-
-        }
+        int score = 0;
+        SoundPlayer p_1 = new SoundPlayer(@"D:\explosion.wav");
+        SoundPlayer p_2 = new SoundPlayer(@"D:\background.wav");
+        PictureBox[] ghost = new PictureBox[4];
+        PictureBox p1 = new PictureBox();
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            p_2.Play();
+            ghost[0] = pictureBox2;
+            ghost[1] = pictureBox3;
+            ghost[2] = pictureBox4;
+            ghost[0].Show();
+            ghost[1].Show();
+            ghost[2].Show();
+            this.Location = new Point(0, 0);
+            this.Size = Screen.PrimaryScreen.WorkingArea.Size;
 
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
         {
+            pictureBox1.Left -= 9;
+            for(int i = 0; i < 3; i++)
+            {
+                if (pictureBox1.Bounds.IntersectsWith(ghost[i].Bounds) && ghost[i].Visible)
+                {
+                    p_1.Play();
+                    this.Controls.Add(p1);
+                    p1.BringToFront();
+                    p1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    p1.Size = new Size(100, 100);
+                    p1.BackColor = Color.Transparent;
+                    p1.Location = ghost[i].Location;
 
+                   p1.Image = Image.FromFile(@"D:\hi.png");
+                    ghost[i].Hide();
+                    p1.Show();
+                    score += 1;
+
+                }
+
+            }
+
+            label1.Text = "Score is     \t" + score;
+           
+        
+        
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            
+            int j = r.Next(50, 500);
+            
+            pictureBox2.Location = new Point(50+j, 10+j);
+            pictureBox3.Location = new Point(150+j, 70+j);
+            pictureBox4.Location = new Point(300+j, 130+j);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -43,54 +84,11 @@ namespace AssignmentoopQ1
             timer2.Start();
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-            Random car1 = new Random();
-            int j = car1.Next(0, 20);
-            pictureBox1.Location = new Point(pictureBox1.Location.X + j, pictureBox1.Location.Y);
-            car1_time++;
-            if (pictureBox1.Left > 915)
-            {
-                timer1.Stop();
-                car1_speed = distance_to_be_covered / car1_time;
-                label1.Text = "The speed of car-1 is:" + Convert.ToString(Math.Round(car1_speed,2));
-            }
-        }
-
-        private void timer2_Tick(object sender, EventArgs e)
-        {
-            Random car1 = new Random();
-            int k= car1.Next(0, 20);
-            pictureBox2.Location = new Point(pictureBox2.Location.X + k, pictureBox2.Location.Y);
-            car2_time++;
-            if (pictureBox2.Left > 915)
-            {
-                timer2.Stop();
-                car2_speed = distance_to_be_covered / car2_time;
-                label2.Text = "The speed of car-2 is:" + Convert.ToString(Math.Round(car2_speed, 2));
-            }
-
-        }
-
-        private void timer3_Tick(object sender, EventArgs e)
-        {
-            if (pictureBox1.Left > 915 && pictureBox2.Left > 915)
-                {
-                timer3.Stop();
-                if (car1_speed > car2_speed)
-                {
-                    MessageBox.Show("The car-1 won" + Environment.NewLine + "speed:\t" +Math.Round(car1_speed,2) +"pixels per tick"+ Environment.NewLine + "TIME\t" + car1_time +"ms");
-                }
-                else
-                {
-                    MessageBox.Show("The car-2 won" + Environment.NewLine + "speed:\t" +Math.Round (car2_speed, 2) + "pixels per tick" + Environment.NewLine + "TIME\t" + car2_time+"ms");
-                }
-            }
-        }
-
-        private void pictureBox2_Click(object sender, EventArgs e)
-        {
-
+            timer1.Stop();
+            timer2.Stop();
+          
         }
     }
 }
